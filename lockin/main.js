ef0b6d04c3d9ee5ddd0bbcf7ebb9d4e507ad57f8
Ethan De Jesus <ethanawesome702@gmail.com>
2023-12-04 21:45:03 -0500

    e

--- lockin/main.js
+++ lockin/main.js
@@ -3,7 +3,19 @@ import './style.css'
 
 
 document.querySelector('#app').innerHTML = `
-  <p> test </p>
+<div class="flip-card">
+    <div class="flip-card-inner">
+        <div class="flip-card-front">
+            <p class="title">FLIP CARD</p>
+            <p>Hover Me</p>
+        </div>
+        <div class="flip-card-back">
+            <p class="title">BACK</p>
+            <p>Leave Me</p>
+        </div>
+    </div>
+</div>
+
   
 `
 
