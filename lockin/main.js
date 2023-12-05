39f5c547ab979105800b6fb2c6a6f58a17c61aac
Ethan De Jesus <124072155+magicianZ@users.noreply.github.com>
2023-12-05 12:22:16 -0500

    ok

--- lockin/main.js
+++ lockin/main.js
@@ -3,38 +3,66 @@ import './style.css'
 
 
 document.querySelector('#app').innerHTML = `
-<div class="flip-card">
-    <div class="flip-card-inner">
-        <div class="flip-card-front">
-            <p class="title">FLIP CARD</p>
-            <p>Hover Me</p>
-        </div>
-        <div class="flip-card-back">
-            <p class="title">BACK</p>
-            <p>Leave Me</p>
-        </div>
-    </div>
-</div>
+
+<button id="gamelookup">Game Lookup </button>
+<div id="container">
+  <form action=""id="forum">
+  <p id="text">this text</p>
+  <button type="button" id="btn">click when done with forms</button>
+  <input type="text" id="input">
+  <label for="text">Attunement</label>
+  </form>
+  
 
   
 `
 
+let Domselectors = {
+  glup:document.querySelector("#gamelookup"),
+  container:document.querySelector("#container")
+}
 
-const URL1 = `https://www.cheapshark.com/api/1.0/games?title=God`;
+const URL1 = `https://www.cheapshark.com/api/1.0/games?title=Lethal`;
 
-async function something(){
+
+
+Domselectors.glup.addEventListener("click",async function(){
   try{
-    let URL = `https://www.cheapshark.com/api/1.0/games?title=God`;
-  let fetching = await fetch(URL);
-  let RealUrl = await fetching.json();
-  console.log('a')
-  RealUrl.forEach((so)=>console.log(so.cheapest))
+    let URL = `https://www.cheapshark.com/api/1.0/games?title=Lethal`;
+    let fetching = await fetch(URL);
+    let RealUrl = await fetching.json();
+    let htmlelements = ""
+    RealUrl.forEach((sm)=>htmlelements+=`
+    <div class="flip-card">
+      <div class="flip-card-inner">
+          <div class="flip-card-front">
+              <p class="title">${sm.external}</p>
+              <p>Hover Me</p>
+          </div>
+          <div class="flip-card-back">
+              <p class="title">BACK</p>
+              <p>Leave Me</p>
+          </div>
+      </div>
+  </div>
+    
+    
+    
+    `
+    
+    
+    )
+    Domselectors.container.innerHTML = htmlelements
+   
 }catch{
   console.log('catched')
 }
-}
 
-something()
+
+})
+
+
+
 
 /* 
 async function gettingdata(URL){
