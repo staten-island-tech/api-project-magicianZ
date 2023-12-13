67d41621b9475cdff83b3d30ba94be40846990ad
Ethan De Jesus <124072155+magicianZ@users.noreply.github.com>
2023-12-13 12:21:39 -0500

    omg

--- lockin/main.js
+++ lockin/main.js
@@ -1,17 +1,16 @@
 import './style.css'
-
+let count = 0;
 
 
 document.querySelector('#app').innerHTML = `
 
 <button id="gamelookup">Game Lookup </button>
 <button id="deallookup"> Deal Lookup </button>
-<button id="npage"> I jjust combusted </button>
+<button id="npage"> Next pag</button>
+<p id="testing"></p>
 <form action=""id="forum">
     <input type="text" id="input">
     <label for="text">What game do you wish to search for?</label>
-    <input type="text" id="input1">
-    <label for="text">testing phase</label>
     </form>
     
 <div id="container">
@@ -30,9 +29,10 @@ let Domselectors = {
   dlup:document.querySelector("#deallookup"),
   npage:document.querySelector("#npage"),
   input1:document.querySelector("#input1"),
+  testing:document.querySelector("#testing"),
   
 }
-
+Domselectors.testing.value = 0
 const URL1 = `https://www.cheapshark.com/api/1.0/games?title=Lethal`;
 const URL2 = `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=3&maxlength=300&format=json`
 const PlayerOwnedGames = ` http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key= F1872C74F497620E49CB760C36AEA826&steamid=76561197960434622&format=json`
@@ -74,7 +74,7 @@ Domselectors.glup.addEventListener("click",async function(){
 
 Domselectors.dlup.addEventListener("click",async function(){
     try{
-      let URLD = `https://www.cheapshark.com/api/1.0/deals?storeID=1`;
+      let URLD = `https://www.cheapshark.com/api/1.0/deals?storeID=${1 + Domselectors.testing.value} `;
       let fetching = await fetch(URLD);
       let RealUrlD = await fetching.json();
       let htmlelements = ""
@@ -106,9 +106,17 @@ Domselectors.dlup.addEventListener("click",async function(){
 
 
 
+ 
 
 
 
 
 
+Domselectors.npage.addEventListener("click",function myFunction() {
+  count++;
+  Domselectors.testing.innerHTML = count;
+  Domselectors.testing.value = count;
+  console.log(Domselectors.testing.value)
   
+  
+})
